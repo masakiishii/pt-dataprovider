eefbf77e55f87e8abf2a41e5d806fd7dcb0b68f3
masakiishii <masaki.ishii511@gmail.com>
2018-07-16 07:18:13 +0900

    initial commit

---
+++ executions-server.js
@@ -0,0 +1,56 @@
+#!/usr/bin/env node
+const io = require('socket.io-client');
+const socket = io("https://io.lightstream.bitflyer.com", { transports: ["websocket"] });
+
+const channelName = "lightning_executions_FX_BTC_JPY";
+socket.on("connect", () => {
+    socket.emit("subscribe", channelName);
+});
+
+
+var WebSocketServer = require('websocket').server;
+var http = require('http');
+
+var server = http.createServer(function(request, response) {
+    console.log((new Date()) + ' Received request for ' + request.url);
+    response.writeHead(404);
+    response.end();
+});
+server.listen(8080, function() {
+    console.log((new Date()) + ' Server is listening on port 8080');
+});
+
+wsServer = new WebSocketServer({
+    httpServer: server,
+    // You should not use autoAcceptConnections for production
+    // applications, as it defeats all standard cross-origin protection
+    // facilities built into the protocol and the browser.  You should
+    // *always* verify the connection's origin and decide whether or not
+    // to accept it.
+    autoAcceptConnections: false
+});
+
+function originIsAllowed(origin) {
+  // put logic here to detect whether the specified origin is allowed.
+  return true;
+}
+
+wsServer.on('request', function(request) {
+    if (!originIsAllowed(request.origin)) {
+      // Make sure we only accept requests from an allowed origin
+      request.reject();
+      console.log((new Date()) + ' Connection from origin ' + request.origin + ' rejected.');
+      return;
+    }
+
+    var connection = request.accept();
+    console.log((new Date()) + ' Connection accepted.');
+    socket.on(channelName, message => {
+        console.log(message);
+	connection.sendUTF(JSON.stringify(message));
+    });
+
+    connection.on('close', function(reasonCode, description) {
+        console.log((new Date()) + ' Peer ' + connection.remoteAddress + ' disconnected.');
+    });
+});
